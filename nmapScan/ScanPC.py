e8661556e0a92a06111d7c99dabc6c79aeec9d3a
Benson <k4232@gcloud.csu.edu.tw>
2024-09-06 16:30:03 +0800

    Update ScanPC.py
    修改為依日期建立結果

--- nmapScan/ScanPC.py
+++ nmapScan/ScanPC.py
@@ -1,6 +1,11 @@
 import os
 import xml.etree.ElementTree as ET
 import pandas as pd
+from datetime import datetime
+
+current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
+output_dir = f"scan_results_{current_time}"
+os.makedirs(output_dir, exist_ok=True)
 
 # 讀取 scanlist.txt 檔案
 with open('scanlist.txt', 'r') as file:
@@ -11,30 +16,29 @@ networks = [network.strip() for network in networks]
 
 # 執行 nmap 掃描並生成 XML 和 HTML 檔案
 for network in networks:
-    output_file = f"{network.replace('/', '_')}.xml" 
-    output_file_html = f"{network.replace('/', '_')}.html"
+    output_file = os.path.join(output_dir, f"{network.replace('/', '_')}.xml") 
+    output_file_html = os.path.join(output_dir, f"{network.replace('/', '_')}.html")
     
     # 執行 nmap 並產生 XML 檔案
     command = f"nmap -sn -v -oX {output_file} {network}"
     os.system(command)
-    # print(f"Executing: {command}")
     
     # 使用 xsltproc 轉換 XML 為 HTML
     command = f"xsltproc -o {output_file_html} nmap-bootstrap.xsl {output_file}"
     os.system(command)
-    # print(f"Executing: {command}")
     
     # 解析 XML 並將 state="up" 的主機 IP 位址寫入 livePC.txt
     tree = ET.parse(output_file)
     root = tree.getroot()
     
     # 清空 livePC.txt 檔案
+    livepc_file = os.path.join(output_dir, 'livePC.txt')
     if network == networks[0]:
-        with open('livePC.txt', 'w') as file:
+        with open(livepc_file, 'w') as file:
             file.write('')
 
     # 搜尋所有 state="up" 的主機，並略過 .0 和 .255 的 IP
-    with open('livePC.txt', 'a') as file:
+    with open(livepc_file, 'a') as file:
         for host in root.findall('host'):
             status = host.find('status')
             if status is not None and status.get('state') == 'up':
@@ -49,7 +53,7 @@ for network in networks:
 print("所有網段掃描完成，並將 state='up' 的主機 IP 位址寫入 livePC.txt")
 
 # 從 livePC.txt 讀取所有 IP 位址
-with open('livePC.txt', 'r') as file:
+with open(livepc_file, 'r') as file:
     live_hosts = file.readlines()
 
 # 清理換行符號
@@ -60,7 +64,7 @@ scan_results = []
 
 # 執行 Nmap 掃描並解析結果
 for ip in live_hosts:
-    output_file = f"{ip}.xml"
+    output_file = os.path.join(output_dir, f"{ip}.xml")
     
     # 執行 Nmap 進行端口掃描
     command = f"nmap -T4 -vvv -oX {output_file} {ip}"
@@ -89,8 +93,8 @@ for ip in live_hosts:
         })
 
 # 使用 pandas 將資料寫入 Excel
+excel_file = os.path.join(output_dir, 'scan_results.xlsx')
 df = pd.DataFrame(scan_results)
-excel_file = 'scan_results.xlsx'
 df.to_excel(excel_file, index=False)
 
 print(f"掃描完成，結果已儲存為 {excel_file}")
